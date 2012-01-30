1d0ff6e694759a9ecdb4096b871d3a27e2fbd228
Vidhur Vohra <dkzjunk@yahoo.com>
2012-01-30 12:58:29 -0500

    inital problem 3

---
+++ src/Problem04.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+
+/**
+ * @author dkz
+ * 
+ */
+public class Problem4 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int one = 0;
+		int two = 0;
+		int largestPalindrome = 0;
+		for (one = 0; one < 1000; one++)
+			for (two = 0; two < 1000; two++)
+				if (isPalindrome(one * two))
+					if (largestPalindrome < one * two)
+						largestPalindrome = one * two;
+		System.out.println(largestPalindrome);
+	}
+
+	private static boolean isPalindrome(int num) {
+		// TODO Auto-generated method stub
+
+		String temp = num + "";
+
+		for (int i = 0; i < temp.length() / 2; i++) {
+			if (temp.charAt(i) != temp.charAt(temp.length() - i - 1))
+				return false;
+		}
+		return true;
+	}
+
+}
---
+++ src/Problem06.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+
+/**
+ * @author dkz
+ * 
+ */
+public class Problem6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double sumsquares = 0;
+
+		for (int i = 1; i < 101; i++)
+			sumsquares += Math.pow(i, 2);
+		double squaresum = 0;
+		int sum = 0;
+		for (int i = 1; i < 101; i++)
+			sum += i;
+		squaresum = Math.pow(sum, 2);
+		System.out.println(sumsquares);
+		System.out.println((int)(sumsquares - (int) squaresum));
+
+	}
+}
