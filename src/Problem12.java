148cd94a0d000c55ee03504828c94c7b17fb1460
Vidhur Vohra <dkzjunk@yahoo.com>
2012-02-06 21:09:22 -0500

    finished problem 12

--- src/Problem12.java
+++ src/Problem12.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.Hashtable;
 
 /**
  * 
@@ -9,13 +11,17 @@ import java.util.ArrayList;
  * 
  */
 public class Problem12 {
+	static Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		slowMethod();
+		// slowMethod();
+		betterMethod();
+		oneMoreBetterMethod();
+
 	}
 
 	public static void slowMethod() {
@@ -40,7 +46,65 @@ public class Problem12 {
 			}
 		return al;
 	}
-	
-	
 
+	public static void betterMethod() {
+
+		boolean haveFound = false;
+		int count = 1;
+		while (!haveFound) {
+			count++;
+			ht = new Hashtable<Integer, Integer>();
+			findPrimesList(count * (count + 1) / 2);
+
+			Enumeration<Integer> e = ht.keys();
+			int num = 1;
+			while (e.hasMoreElements()) {
+				num = num * (1 + ht.get(e.nextElement()));
+			}
+			if (num >= 500)
+				break;
+		}
+		System.out.println(count * (count + 1) / 2);
+	}
+
+	public static void oneMoreBetterMethod() {
+
+		boolean haveFound = false;
+		int currNum = 1;
+		int count = 2;
+		while (!haveFound) {
+			ht = new Hashtable<Integer, Integer>();
+			currNum += count;
+			count++;
+			findPrimesList(currNum);
+
+			Enumeration<Integer> e = ht.keys();
+			int num = 1;
+			while (e.hasMoreElements()) {
+				num = num * (1 + ht.get(e.nextElement()));
+			}
+			if (num >= 500)
+				break;
+		}
+		System.out.println(currNum);
+	}
+
+	public static void findPrimesList(int n) {
+		int currMax = n;
+		for (int i = 2; i <= currMax; i++) {
+			if (currMax % i == 0) {
+				if (ht.get(i) == null)
+					ht.put(i, 1);
+				else {
+					int temp = ht.get(i);
+					temp++;
+					ht.put(i, temp);
+				}
+
+				currMax = currMax / i;
+				i = 1;
+			}
+		}
+		int x = 0;
+	}
 }
