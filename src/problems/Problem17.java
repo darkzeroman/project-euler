c46410c5678d06faad41488869eb2a2339c03775
Vidhur Vohra <dkzjunk@yahoo.com>
2012-02-08 23:45:58 -0500

    problem 17

---
+++ src/problems/Problem17.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package problems;
+
+/**
+ * @author dkz
+ * 
+ */
+public class Problem17 {
+	// Not worth it for me at the moment, seems a lot of work for a simple
+	// answer. And there doesn't seem to be a "simple" way to solve this
+
+	/**
+	 * 
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void slowMethod() {
+
+	}
+
+	public static void getNumLetters(int num) {
+		// if (num <= 10)
+	}
+
+}
